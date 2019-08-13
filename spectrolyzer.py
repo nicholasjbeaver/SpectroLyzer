5eccd688f8ee7df1dfc9b46c0e06a33f4dc3363e
Nick Beaver <mail@nickbeaver.net>
2019-08-13 10:57:48 -0700

    planning code with comments

--- spectrolyzer.py
+++ spectrolyzer.py
@@ -1,2 +1,18 @@
 import pandas as pd
 import numpy as np
+
+# Search for csv files in data/
+
+# make a dictionary of csv files and how they will be referenced
+
+# function to display table at maximum wavelength
+
+# based on csv chosen, make a dataframe
+
+# filter by sample if user chooses to
+
+# ask user to insert a specific wavelength to look at (or no)
+
+# ask user to insert a wavelength range to find a max (or no)
+
+# display raw data if prompted
